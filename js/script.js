1a83063a810d4bb727db2e9d3324ecb8e8301887
nhatlan <vanphamnhatlan93@gmail.com>
2015-08-19 13:27:30 +0700

    no change

--- js/script.js
+++ js/script.js
@@ -3,17 +3,22 @@ $(function(){
 	// Init Controller
 		var scrollMagicController = new ScrollMagic.Controller(); 
 
-// 	// Add animation 
-// 	var tween = TweenMax.to('#opening', 0.5, {
-//     backgroundColor: 'rgb(255, 39, 46)',
-//     scale: 1,
-//     rotation: 20
-// });
+	// Add animation 
+	// var tween = TweenMax.to('.blur', 0.5, {
+ //    backgroundColor: 'rgb(255, 39, 46)',
+ //    scale: 1,
+ //    rotation: 20
+	// });
 	//Create Scene 
 		var scene = new ScrollMagic.Scene({
     triggerElement: '#opening',
+    duration: 0
   })
-	.setTween(".blur", {scale: 2})
+	.setTween(TweenMax.to('.blur', 0.5, {
+    backgroundColor: 'rgb(255, 39, 46)',
+    scale: 1,
+    rotation: 20
+	}))
 	.addTo(scrollMagicController)
 
 	  // Add debug indicators fixed on right side
