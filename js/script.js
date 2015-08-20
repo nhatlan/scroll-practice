68ac35d43f5339048599ffe3413f30ad6e53f965
nhatlan <vanphamnhatlan93@gmail.com>
2015-08-20 16:16:00 +0700

    js run

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 // $(function() {
 // 	var controller = new ScrollMagic.Controller();
 // 	var scene = new ScrollMagic.Scene({
-// 		triggerElement: "#opening"
-// 	})
-// 	.setTween("#opening", 0.3, {scale: 2.5})
-// 	.addIndicators({name: "1 (duration: 0)"})
-// 	.addTo(controller);
-// });
+// 	triggerElement: "#opening"
+// })
+// .setTween("#opening", 0.3, {scale: 2.5})
+// .addIndicators({name: "1 (duration: 0)"})
+// .addTo(controller);
