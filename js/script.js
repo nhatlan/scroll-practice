3556989d080bba7470fdfa8f6f338889705fe21e
nhatlan <vanphamnhatlan93@gmail.com>
2015-08-21 10:50:16 +0700

    feat js run/ changing background start

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-// $(function() {
-// 	var controller = new ScrollMagic.Controller();
-// 	var scene = new ScrollMagic.Scene({
-// 	triggerElement: "#opening"
-// })
-// .setTween("#opening", 0.3, {scale: 2.5})
-// .addIndicators({name: "1 (duration: 0)"})
-// .addTo(controller);
+$(function(){
+	// Init Scroll Magic 
+	var controller = new ScrollMagic.Controller(); 
+	// Opening Animation 
+	var tween = TweenMax.to(".opening-text", 0.2, 
+					{
+						'-webkit-filter':'blur(3px)'
+					});
+	//Build Open Scene 
+	var titleScene = new ScrollMagic.Scene({
+		triggerElements: "#title",
+		duration: 500
+	})
+	.setPin("#pin1")
+	.setClassToggle("#title", "title")
+	.setTween(tween)
+	.addIndicators({name :"1 {duration:0}"})
+	.addTo(controller); 
+
+	var scene1 = new ScrollMagic.Scence({
+		triggerElements: "#scene1",
+		duration: 500
+	})
+	.setClassToggle("#scene1", "scene1")
+	.addIndicators({name :"2 {duration:500}"})
+	.addTo(controller); 
+});
