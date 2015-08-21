ba24af690bdaf6ca79b3486d444fd9fb65b23c80
nhatlan <vanphamnhatlan93@gmail.com>
2015-08-21 11:08:40 +0700

    feat title run, title section run, section 2 bug unfixed

--- js/script.js
+++ js/script.js
@@ -2,26 +2,28 @@ $(function(){
 	// Init Scroll Magic 
 	var controller = new ScrollMagic.Controller(); 
 	// Opening Animation 
-	var tween = TweenMax.to(".opening-text", 0.2, 
+	var tweenTitle = TweenMax.to(".title-text", 0.2, 
 					{
 						'-webkit-filter':'blur(3px)'
 					});
-	//Build Open Scene 
+	//Build Title Scene 
 	var titleScene = new ScrollMagic.Scene({
 		triggerElements: "#title",
-		duration: 500
 	})
 	.setPin("#pin1")
-	.setClassToggle("#title", "title")
-	.setTween(tween)
+	.setTween(tweenTitle)
 	.addIndicators({name :"1 {duration:0}"})
 	.addTo(controller); 
 
+	//Test Scene 1 Animation
+	var tweenScene1 = TweenMax.to(".scene1-text", 0.2, {scale: 2});
+
+	//Build Scene1 
 	var scene1 = new ScrollMagic.Scence({
 		triggerElements: "#scene1",
 		duration: 500
 	})
-	.setClassToggle("#scene1", "scene1")
+	.setTween(tweenScene1)
 	.addIndicators({name :"2 {duration:500}"})
 	.addTo(controller); 
 });
